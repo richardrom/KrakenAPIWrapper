949e8664192060c5f76e62abb4a74e48efe779e0
Richard Romero <qricardo@outlook.com>
2026-09-10 14:51:53 -0600

    init library macros

--- include/krakendefs.hpp
+++ include/krakendefs.hpp
@@ -13,4 +13,35 @@
 #ifndef KRAKENAPIWRAPPER_KRAKENLIB_HPP
 #define KRAKENAPIWRAPPER_KRAKENLIB_HPP
 
+#ifdef USE_EXPORT_LIBRARY
+#if defined(_WIN32)
+#define KRAKEN_LIBRARY_HIDDEN
+#if defined(M_MSVC)
+#define KRAKEN_LIBRARY __declspec(dllexport)
+#else
+#define KRAKEN_LIBRARY __attribute__((dllexport))
+#endif
+#else
+#define KRAKEN_LIBRARY        __attribute__((visibility("default")))
+#define KRAKEN_LIBRARY_HIDDEN __attribute__((visibility("hidden")))
+#endif
+#else
+#if defined(_WIN32)
+#if defined(M_MSVC)
+#define KRAKEN_LIBRARY __declspec(dllimport)
+#else
+#define KRAKEN_LIBRARY __attribute__((dllimport))
+#endif
+#else
+#define KRAKEN_LIBRARY
+#endif
+#define KRAKEN_LIBRARY_HIDDEN
+#endif /*USE_EXPORT_LIBRARY*/
+
+namespace kraken
+{
+
+}
+
+
 #endif // KRAKENAPIWRAPPER_KRAKENLIB_HPP
